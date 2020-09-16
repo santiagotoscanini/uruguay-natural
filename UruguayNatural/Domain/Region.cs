cf855528454525b934d0ee5a1fc886026a13c283
Sofia Tejerina <sofiatejerina23@gmail.com>
2020-09-16 13:30:01 -0300

    Create project. Using tdd create tourist spot class in domain

---
+++ UruguayNatural/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinesLogic
+{
+    public static class Region
+    {
+        public const string Metropolitan = "Región metropolitana";
+        public const string East = "Región Este";
+        public const string SouthCentral = "Región Centro Sur";
+        public const string NorthCoast = "Región Litoral Norte";
+        public const string PaintedBirdCorridor = "Región \"Corredor Pájaros Pintados\"";
+    }
+}
