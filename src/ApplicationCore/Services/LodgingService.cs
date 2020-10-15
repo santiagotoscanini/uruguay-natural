ed622885c9587ad2552d1e10eac3a382304d9ece
santiago toscanini <toscaninisantiago@gmail.com>
2020-10-15 17:30:26 -0300

    refactoring

--- src/ApplicationCore/Services/LodgingService.cs
+++ src/ApplicationCore/Services/LodgingService.cs
@@ -1,8 +1,8 @@
 ﻿using ApplicationCoreInterface.Services;
 using Entities;
 using InfrastructureInterface.Data.Repositories;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ApplicationCore.Services
 {
@@ -10,14 +10,24 @@ namespace ApplicationCore.Services
     {
         private readonly ILodgingRepository _repository;
 
-        public LodgingService(ILodgingRepository repository)
+        private ITouristPointService _touristPointService;
+
+        public LodgingService(ILodgingRepository repository, ITouristPointService touristPointService)
         {
             _repository = repository;
+            _touristPointService = touristPointService;
         }
 
-        public Lodging Add(Lodging lodging)
+        public Lodging Add(Lodging lodging, int touristPointId)
         {
+            lodging.TouristPoint = GetTouristPoint(touristPointId);
+            
             return _repository.Add(lodging);
+        }
+
+        private TouristPoint GetTouristPoint(int touristPointId)
+        {
+            return _touristPointService.GetAll().FirstOrDefault(tp => tp.Id == touristPointId);
         }
 
         public void Delete(int lodgingId)
